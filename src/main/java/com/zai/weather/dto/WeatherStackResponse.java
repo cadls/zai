7309c90c79e9790750bec3685b7dffb53b16ee48
cadls <cheskaann7@gmail.com>
2025-05-06 15:26:10 +0800

    update dto

--- src/main/java/com/zai/weather/dto/WeatherStackResponse.java
+++ src/main/java/com/zai/weather/dto/WeatherStackResponse.java
@@ -1,17 +1,14 @@
 package com.zai.weather.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherStackResponse {
 
     private Current current;
 
     @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Current {
 
         @JsonProperty("wind_speed")
